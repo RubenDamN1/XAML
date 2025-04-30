5d699a01b03179a777003e817115b1c43f7e2da7
RubenDamN1 <103996489+RubenDamN1@users.noreply.github.com>
2025-04-30 10:43:41 +0200

    Primer esqueleto con IA
    Primer commit

---
+++ Activ_ev_2cuat_RubenAF/Activ_ev_2cuat_RubenAF/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Activ_ev_2cuat_RubenAF
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnCategoria1_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void btnCategoria2_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void btnCategoria3_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void Abrir_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void Guardar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void Borrar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void Imprimir_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+       
+     }
+    
+}
